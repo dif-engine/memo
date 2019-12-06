359f5b3879097b04e11fba4ebb9835f9ab223b38
Ozaki Ryushi <ozaki.ryushi@gmail.com>
2019-12-06 18:39:06 +0900

    wip

--- cpp/a.cc
+++ cpp/a.cc
@@ -1,6 +1,6 @@
 /*---------------------------------------------
- *  iterator で，列の最後の要素だけ特別扱いしたい
- *   ---------------------------------------------*/
+   iterator で，列の最後の要素だけ特別扱いしたい
+ *---------------------------------------------*/
 
 #include <vector>
 #include <iostream>
@@ -10,27 +10,27 @@ using namespace std;
 
 int main()
 {
-		typedef vector<string> svector;
-			
-			svector a{"first", "second", "third", "fourth", "fifth", "sixth", "last"};
+	typedef vector<string> svector;
 
-				auto end = ++(a.rbegin());
-					
-					for(auto i = a.begin(); i != a.end(); ++i)
-							{
-										cout << *i << flush;
-												if(i != end.base()){ 
-																cout << "," << flush; 
-																		}else{
-																						cout << ";" << endl;
-																								}
-													}
-						return 0;	
+	svector a{"first", "second", "third", "fourth", "fifth", "sixth", "last"};
+
+	auto end = ++(a.rbegin());
+
+	for(auto i = a.begin(); i != a.end(); ++i)
+	{
+		cout << *i << flush;
+		if(i != end.base()){ 
+			cout << "," << flush; 
+		}else{
+			cout << ";" << endl;
+		}
+	}
+	return 0;	
 }
 
-/*
- * 出力：
- * first,second,third,fourth,fifth,sixth,last;
- * */
+/*-----------------------------------------------
+ 出力：
+ first,second,third,fourth,fifth,sixth,last;
+-----------------------------------------------*/
 
 
